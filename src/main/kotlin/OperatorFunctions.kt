da6d4735b058881deaa05d85d4b02edd187d0788
thigorqueiroz <thigorqueiroz@gmail.com>
2021-09-01 14:28:12 -0300

    operator fun

---
+++ src/main/kotlin/OperatorFunctions.kt
@@ -0,0 +1,11 @@
+
+fun main() {
+    operator fun Int.times(str: String) = str.repeat(this)
+    println(2 * "Bye ")
+
+    operator fun String.get(range: IntRange) = substring(range)
+
+    val str = "Always forgive your enemies; nothing annoys them so much."
+    println(str[0..14])
+}
+
